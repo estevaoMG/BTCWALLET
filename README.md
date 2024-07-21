68af2ab3930d85e879c03d562fad9346f9c8dd94
estevaoMG <estevao.gouveia@hotmail.com>
2024-07-20 23:26:30 -0300

    Finalizando o projeto.

--- README.md
+++ README.md
@@ -7,3 +7,32 @@
 - Javascript
 - Electrum Bitcoin Wallet
 - Bitcoin Faucet Generator
+
+## Iniciando o projeto:
+
+- npm init -y
+- npm install bip39 bip32@2.0.6 bitcoinjs-lib --save
+- cd .\src\
+- node .\createWallet.js
+
+## Instalar Electrum
+
+- Baixar e instalar a partir do site: electrum.org
+
+## Pesquisar endereço da carteira gerada (Testnet)
+
+- https://live.blockcypher.com/btc-testnet/
+
+## Adquirir criptomoeda de teste - Bitcoin Faucet
+
+- https://coinfaucet.eu/en/btc-testnet/
+
+## Electrum - Renomear caminho de executável do ícone da área de trabalho, acrescentando o seguinte código:
+
+- caminho do arquivo... --testnet
+
+## Abrir Elecrtum criando uma nova carteira, importando a chave privada.
+
+## Enviar criptomoedas para um novo endereço.
+
+## Transação concluída!
---
+++ src/createWallet.js
@@ -0,0 +1,33 @@
+//importando as dependências
+const bip32 = require('bip32')
+const bip39 = require('bip39')
+const bitcoin = require('bitcoinjs-lib')
+
+//definir a rede
+//bitcoin - rede principal - mainnet
+//testnet - rede de teste
+const network = bitcoin.networks.testnet
+
+//derivação de carteiras HD - 1 testnet - 0 mainnet (bitcoin)
+const path = `m/49'/1'/0'/0`
+
+//criando mnemonic para a seed (palavras de senha)
+let mnemonic = bip39.generateMnemonic()
+const seed = bip39.mnemonicToSeedSync(mnemonic)
+
+//criando a raiz da carteira HD
+let root = bip32.fromSeed(seed, network)
+
+//criando uma conta - par pvt-pub keys
+let accont = root.derivePath(path)
+let node = accont.derive(0).derive(0)
+
+let btcAddress = bitcoin.payments.p2pkh({
+    pubkey: node.publicKey,
+    network: network,
+}).address
+
+console.log("Carteira gerada")
+console.log("Endereço:", btcAddress)
+console.log("Chave privada:", node.toWIF())
+console.log("Seed:", mnemonic)
